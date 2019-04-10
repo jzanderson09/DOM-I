94c856b54794905c4b9c3e043b75ce0bb690c3ba
jzanderson09 <jzanderson09@gmail.com>
2019-04-10 09:13:30 -0600

    Finished navigation, moving on to h1, button and img in the header.

--- js/index.js
+++ js/index.js
@@ -40,3 +40,33 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//Assignment starts here:
+
+//Header:
+
+//Navigation:
+/*
+setting siteContent.nav.[nav-items] to variables.
+seems more like a long-term solution to setting
+the nav, not hardcoded to text.*/
+
+//Assigning variable names to site content in the objects in js file:
+let firstNav = siteContent.nav["nav-item-1"];
+let secondNav = siteContent.nav["nav-item-2"];
+let thirdNav = siteContent.nav["nav-item-3"];
+let fourthNav = siteContent.nav["nav-item-4"];
+let fifthNav = siteContent.nav["nav-item-5"];
+let sixthNav = siteContent.nav["nav-item-6"];
+//Grabbing anchor tags and assigning respectively:
+let navBar = document.querySelectorAll('a');
+navBar[0].textContent = firstNav;
+navBar[1].textContent = secondNav;
+navBar[2].textContent = thirdNav;
+navBar[3].textContent = fourthNav;
+navBar[4].textContent = fifthNav;
+navBar[5].textContent = sixthNav;
+
+//Lower Header(text, button and img):
+
+let headerText = siteContent.nav
